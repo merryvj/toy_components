7404f66dd687fbdb03657b583b2fa3e3b36cbc10
Mary Jiang <41601131+merryvj@users.noreply.github.com>
2022-02-04 23:23:12 -0500

    added tracker

---
+++ 17. expense calcuator/script.js
@@ -0,0 +1,9 @@
+const totalEl = document.getElementById("total");
+const historyEl = document.getElementById("history");
+
+const entryEl = document.getElementById("entry");
+
+function addEntry() {
+    let val = entryEl.value;
+    console.log(val);
+}
