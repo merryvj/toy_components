604753088ae88050e8a095d6776b13e88fc84fcd
Mary Jiang <41601131+merryvj@users.noreply.github.com>
2022-02-05 10:12:35 -0500

    git commit -m calculate values

--- 17. expense calcuator/script.js
+++ 17. expense calcuator/script.js
@@ -2,8 +2,63 @@ const totalEl = document.getElementById("total");
 const historyEl = document.getElementById("history");
 
 const entryEl = document.getElementById("entry");
+const submitBtn = document.getElementById("submit");
 
-function addEntry() {
-    let val = entryEl.value;
-    console.log(val);
+
+let total = 0;
+
+let entries = [
+    {
+        id: 1,
+        amount: -100,
+        desc: "a bottle of water"
+    },
+    {
+        id: 2,
+        amount: 100,
+        desc: "a bottle of MILK"
+    },
+    {
+        id: 3,
+        amount: 12,
+        desc: "a bottle of juice"
+    },
+]
+
+function makeEntryEl(entry) {
+    
+    const sign = entry.amount < 0 ? "-" : "+";
+    const item = document.createElement("li");
+    item.classList.add(entry.amount < 0 ? "minus" : "plus");
+    item.innerHTML = `
+        ${entry.desc} <span>${sign}${Math.abs(entry.amount)}</span>
+    `;
+
+    historyEl.append(item);
 }
+
+function updateValues(){
+    let amounts = entries.map(entry => entry.amount);
+
+    let total = amounts.reduce((acc, entry) => (acc += entry), 0);
+
+    let income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0);
+
+    let expense = amounts
+        .filter(item => item < 0)
+        .reduce((acc, item) => (acc =+ item), 0);
+    
+}
+
+
+
+function init(){
+    historyEl.innerHTML = '';
+    entries.forEach(entry => makeEntryEl(entry));
+    
+    updateValues();
+}
+
+document.onload = init();
