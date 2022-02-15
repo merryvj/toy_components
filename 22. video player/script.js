2f38de00b3c6a2e89ac67656a80af5a5dd73fc2d
Mary Jiang <41601131+merryvj@users.noreply.github.com>
2022-02-14 23:22:08 -0500

    WOAH there's a webcam as a mirror!!

--- 22. video player/script.js
+++ 22. video player/script.js
@@ -6,7 +6,6 @@ const progress = document.querySelector("#progress");
 const speed = document.querySelector("#speed");
 const timestamp = document.querySelector("#timestamp");
 
-
 video.addEventListener("timeupdate", updateProgress);
 playBtn.addEventListener("click", toggleVideoPlay);
 mirrorBtn.addEventListener("click", toggleVideoMirror);
@@ -14,59 +13,74 @@ progress.addEventListener("change", setVideoProgress);
 speed.addEventListener("change", updateVideoSpeed);
 
 function updateProgress() {
-    progress.value = (video.currentTime / video.duration) * 100;
-    setTimestamp();
+  progress.value = (video.currentTime / video.duration) * 100;
+  setTimestamp();
 }
 
 function setVideoProgress() {
-    video.currentTime = (+progress.value * video.duration) / 100;
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 function setTimestamp() {
-    let minutes = Math.floor(video.currentTime / 60);
-    let seconds = Math.floor(video.currentTime - minutes * 60);
-    let minuteValue;
-    let secondValue;
+  let minutes = Math.floor(video.currentTime / 60);
+  let seconds = Math.floor(video.currentTime - minutes * 60);
+  let minuteValue;
+  let secondValue;
 
-    if (minutes < 10) {
-        minuteValue = '0' + minutes;
-    } else {
-        minuteValue = minutes;
-    }
+  if (minutes < 10) {
+    minuteValue = "0" + minutes;
+  } else {
+    minuteValue = minutes;
+  }
 
-    if (seconds < 10) {
-        secondValue = '0' + seconds;
-    } else {
-        secondValue = seconds;
-    }
+  if (seconds < 10) {
+    secondValue = "0" + seconds;
+  } else {
+    secondValue = seconds;
+  }
 
-    timestamp.textContent = `${minuteValue}:${secondValue}`;
+  timestamp.textContent = `${minuteValue}:${secondValue}`;
 }
 
 function stopVideo() {
-    video.pause();
-    video.currentTime = 0;
+  video.pause();
+  video.currentTime = 0;
 }
 function toggleVideoPlay() {
-    if(video.paused) {
-      playBtn.innerHTML = '<i class="lni lni-pause"></i>'
-      video.play();
-    } else {
-      playBtn.innerHTML = '<i class="lni lni-play"></i>';
-      video.pause();
-    }
+  if (video.paused) {
+    playBtn.innerHTML = '<i class="lni lni-pause"></i>';
+    video.play();
+  } else {
+    playBtn.innerHTML = '<i class="lni lni-play"></i>';
+    video.pause();
+  }
 }
 
 function toggleVideoMirror() {
-    video.classList.toggle("mirrored");
+  video.classList.toggle("mirrored");
 }
 
-
 function updateVideoSpeed() {
-    let rate = +speed.value;
-    video.playbackRate = rate;
+  let rate = +speed.value;
+  video.playbackRate = rate;
 }
 
 //remove default browser controls from video
 video.removeAttribute("controls");
 controls.style.visibility = "visible";
+
+const webcam = document.querySelector("#webcam");
+
+navigator.mediaDevices
+  .getUserMedia({video: true })
+  .then(function (stream) {
+    if ("srcObject" in webcam) {
+        webcam.srcObject = stream;
+    } 
+    webcam.onloadedmetadata = function (e) {
+        webcam.play();
+    };
+  })
+  .catch(function (err) {
+    console.log(err.name + ": " + err.message);
+  });
