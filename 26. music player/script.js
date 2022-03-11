2296bc2b8861a5cfcb03e7345aee553dde9f3f8e
Mary Jiang <41601131+merryvj@users.noreply.github.com>
2022-03-10 23:40:09 -0500

    added elements to script

---
+++ 26. music player/script.js
@@ -0,0 +1,7 @@
+const playBtn = document.querySelector("#play");
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+
+const progressEl = document.querySelector("#progress");
+const coverImg = document.querySelector("#cover");
+const audio = document.querySelector("#audio");
