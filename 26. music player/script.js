196e437653b73f8abcec6c380f4d07fe8ea80722
Mary Jiang <41601131+merryvj@users.noreply.github.com>
2022-03-12 00:06:19 -0500

    disk arm animation

--- 26. music player/script.js
+++ 26. music player/script.js
@@ -3,5 +3,13 @@ const prevBtn = document.querySelector("#prev");
 const nextBtn = document.querySelector("#next");
 
 const progressEl = document.querySelector("#progress");
+const armEl = document.querySelector("#arm");
 const coverImg = document.querySelector("#cover");
 const audio = document.querySelector("#audio");
+
+
+playBtn.addEventListener("click", () => {
+    armEl.classList.toggle("rotated");
+    coverImg.style.animationPlayState = "running";
+
+})
